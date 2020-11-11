3be461848add95ff819846a365e817c151708d51
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-11 01:51:48 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,16 +2,17 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def self.sentence?(str)
+    str = str
+    str.end_with?(".")
   end
 
   def question?
-
+    str.end_with?("?")
   end
 
   def exclamation?
-
+    str.end_with?("!")
   end
 
   def count_sentences
